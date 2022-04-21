21c8eda58af1481fd75edaf4cc01888a308b845c
Barry Wood <barrywood721@gmail.com>
2022-04-21 15:45:54 -0400

    Taxes: Add gross income computation
    Simulations use after tax income, which requires the ability to compute
    how much to withdraw from a taxable asset to achieve a given after tax
    income.  This update adds a 'gross income for net income' function to
    both the tax_bracket and taxes objects to support this need.

--- taxes.py
+++ taxes.py
@@ -54,6 +54,26 @@ class tax_brackets(object):
         tax = tax + (remainder * self.top_bracket_tax_rate)
         return tax
 
+    def gross_income_for_net_income(self, net_income):
+        low_gross = net_income
+        high_gross = net_income / (1-self.top_bracket_tax_rate)
+        mid_gross = (low_gross + high_gross)/2
+
+        taxes = self.compute_tax(mid_gross)
+        cnt = 0
+        while (abs(mid_gross - taxes - net_income) > 0.01):
+            ## Mid gross is too low, bump it up
+            if (mid_gross - taxes) < net_income:
+                low_gross = mid_gross
+            else:
+            ## Mid gross is too high, bump it down
+                high_gross = mid_gross
+            mid_gross = (low_gross + high_gross)/2
+            taxes = self.compute_tax(mid_gross)
+            cnt += 1
+
+        return round(mid_gross,2), round(taxes, 2)
+
 class taxes(object):
     provinces = ["BC", "AB", "SK", "MB", "ON", "QC", "NB", "PE", "NS", "NL"]
 
@@ -87,10 +107,39 @@ class taxes(object):
     def _provincial_taxes(self, income):
         return self.provincial_tax_brackets[self.location].compute_tax(income)
 
+    def _max_combined_tax_rate(self):
+        max_fed = self.federal_tax_brackets.top_bracket_tax_rate
+        max_prov = self.provincial_tax_brackets[self.location].top_bracket_tax_rate
+        return max_fed + max_prov
+
     def compute_taxes(self, income=0.0):
         if income <= 0:
             return 0.0, 0.0
-        return self._federal_taxes(income), self._provincial_taxes(income)
+        fed_taxes = round(self._federal_taxes(income), 2)
+        prov_taxes = round(self._provincial_taxes(income), 2)
+        return fed_taxes, prov_taxes
+
+    def gross_income_for_net_income(self, net_income):
+        low_gross = net_income
+        high_gross = net_income / (1-self._max_combined_tax_rate())
+        mid_gross = (low_gross + high_gross)/2
+
+        fed, prov = self.compute_taxes(mid_gross)
+        cnt = 0
+        taxes = fed + prov
+        while (abs(mid_gross - taxes - net_income) > 0.01):
+            ## Mid gross is too low, bump it up
+            if (mid_gross - taxes) < net_income:
+                low_gross = mid_gross
+            else:
+            ## Mid gross is too high, bump it down
+                high_gross = mid_gross
+            mid_gross = (low_gross + high_gross)/2
+            fed, prov = self.compute_taxes(mid_gross)
+            taxes = fed + prov
+            cnt += 1
+
+        return round(mid_gross,2), round(taxes, 2)
 
 def create_parser():
     parser = OptionParser(description="Quick check for tax computation.")
@@ -116,8 +165,18 @@ def main(argv = None):
 
     obj = taxes()
     fed, prov = obj.compute_taxes(income=options.income)
-    print("Income    : %8.2f\nFederal   : %8.2f\nProvincial: %8.2f\n" %
+    print("Income    : %8.2f\nFederal   : %8.2f\nProvincial: %8.2f" %
             (options.income, fed, prov))
+    gross_fed, gross_fed_taxes = obj.federal_tax_brackets.gross_income_for_net_income(options.income)
+    comp_fed_taxes = obj.federal_tax_brackets.compute_tax(gross_fed)
+    gross_prov, gross_prov_taxes = obj.provincial_tax_brackets["ON"].gross_income_for_net_income(options.income)
+    comp_prov_taxes = obj.provincial_tax_brackets["ON"].compute_tax(gross_prov)
+    print("Federal : Gross Income %8.2f   Taxes %8.2f Comp %8.2f" % (gross_fed, gross_fed_taxes, comp_fed_taxes))
+    print("Ontario : Gross Income %8.2f   Taxes %8.2f Comp %8.2f" % (gross_prov, gross_prov_taxes, comp_prov_taxes))
+
+    gross, tot_tax = obj.gross_income_for_net_income(options.income)
+    comp_fed_tax, comp_prov_tax = obj.compute_taxes(gross)
+    print("Combined: Gross Income %8.2f   Taxes %8.2f Comp %8.2f" % (gross, tot_tax, comp_fed_tax+comp_prov_tax))
 
 if __name__ == '__main__':
     sys.exit(main())
